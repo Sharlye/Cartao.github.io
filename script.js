fbeb8eb2cfea8ba8205cd2032473ea36701c2b77
Sharlye <sharlyej64@gmail.com>
2023-06-07 13:24:08 -0300

    adjust image

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function toggleMode() {
     const img = document.querySelector("#profile img")
 
     if(html.classList.contains("light")) {
-        img.setAttribute("src", "./Files/Avatar.png")
+        img.setAttribute("src", "./Files/Avatar.jpg")
     } else {
-        img.setAttribute("src", "./Files/Avatar.png")
+        img.setAttribute("src", "./Files/Avatar.jpg")
     }
 }
 
